28f6d6fc83ae9cd4db0a2b9f04eb5a8b89431c75
Chris Jenks <38247910+ChrisJenks96@users.noreply.github.com>
2018-05-08 18:48:44 +0100

    v0.02
    - joystick poll was returning the wrong value based on being active or not.
    
    - joystick setup's cooperation level is now DISCL_NONEXCLUSIVE | DISCL_BACKGROUND which seems to work consistently, opposed to DISCL_NONEXCLUSIVE | DISCL_FOREGROUND
    
    - added joystick specific (saitek x7-38) functions.

--- saitek_joystick.h
+++ saitek_joystick.h
@@ -1,36 +1,38 @@
 #ifndef SAITEK_JOYSTICK_H
 #define SAITEK_JOYSTICK_H
 
-//This was tested on Saitek X7-38
-#include <dinput.h>
+#ifdef _WIN32
+	//This was tested on Saitek X7-38
+	#include <dinput.h>
 
-//how to use...
-/*
-#define SAITEK_X7_38_ENABLE
-#include "saitek_joystick.h"
+	//how to use...
+	/*
+	#define SAITEK_X7_38_ENABLE
+	#include "saitek_joystick.h"
 
-if (!FAILED(joystick_setup()))
-{
-	DIJOYSTATE2 js;
-	while (true)
-		joystick_poll(&js);
-}
+	if (!FAILED(joystick_setup()))
+	{
+		DIJOYSTATE2 js;
+		while (true)
+			joystick_poll(&js);
+	}
 
-joystick_destroy();*/
+	joystick_destroy();*/
 
-static LPDIRECTINPUT8 di;
-static LPDIRECTINPUTDEVICE8 joystick;
+	static LPDIRECTINPUT8 di;
+	static LPDIRECTINPUTDEVICE8 joystick;
 
-BOOL CALLBACK joystick_enum_callback(const DIDEVICEINSTANCE* instance, VOID* context);
-BOOL CALLBACK joystick_enum_axis_callback(const DIDEVICEOBJECTINSTANCE* instance, VOID* context);
-HRESULT joystick_setup();
-HRESULT joystick_poll(DIJOYSTATE2* js);
-void joystick_destroy();
+	BOOL CALLBACK joystick_enum_callback(const DIDEVICEINSTANCE* instance, VOID* context);
+	BOOL CALLBACK joystick_enum_axis_callback(const DIDEVICEOBJECTINSTANCE* instance, VOID* context);
+	HRESULT joystick_setup();
+	bool joystick_poll(DIJOYSTATE2* js);
+	void joystick_destroy();
 
-#ifdef SAITEK_X7_38_ENABLE
-	LONG joystick_get_throttle(DIJOYSTATE2* js);
-	LONG joystick_get_x(DIJOYSTATE2* js);
-	LONG joystick_get_y(DIJOYSTATE2* js);
+	#ifdef SAITEK_X7_38_ENABLE
+		LONG joystick_get_throttle(DIJOYSTATE2* js);
+		LONG joystick_get_x(DIJOYSTATE2* js);
+		LONG joystick_get_y(DIJOYSTATE2* js);
+	#endif
 #endif
 
 #endif
